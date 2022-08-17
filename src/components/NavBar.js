a65d473d8368bbea68d8e4cdfbbf8ab23f9c0b9a
Jacoby Yliniemi <cobyyliniemi@gmail.com>
2022-08-17 08:50:34 -0500

    Added to navbar and built project

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,15 @@ import Typography from "@mui/material/Typography";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Book from "@mui/icons-material/Book";
+import MenuItem from "@mui/material/MenuItem";
+
+const Item = ({ text, onClick })=>{
+    return (
+        <MenuItem onClick={onClick} sx={{ml: 1}}>
+            <Typography textAlign="center">{text}</Typography>
+        </MenuItem>
+    );
+}
 
 const NavBar = ()=>{
     return (
@@ -11,6 +20,10 @@ const NavBar = ()=>{
                 <Typography variant="h6">
                     Book Store
                 </Typography>
+                <Item text="Login" />
+                <Item text="Register" />
+                <Item text="Edit Profile" />
+                <Item text="Something else" />
             </Toolbar>
         </AppBar>
     );
